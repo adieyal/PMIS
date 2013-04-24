16ffc37e4c9ebe9b339531c5e1f49d8b232ec21e
Marlin Forbes <marlinf@datashaman.com>
2013-04-24 19:20:16 +0200

    Add explicit path

--- bin/collectstatic.sh
+++ bin/collectstatic.sh
@@ -6,4 +6,4 @@ workon pmis
 cd /home/pmis/deploy
 
 # XXX: Test settings are the only ones I can get to work
-python manage.py collectstatic --settings=project.settings.test --noinput --clear
+/home/pmis/.virtualenvs/pmis/bin/python manage.py collectstatic --settings=project.settings.test --noinput --clear
