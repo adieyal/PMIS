a8029e3689204e0b3dc578d4237f42f2f401b556
Marlin Forbes <marlinf@datashaman.com>
2013-04-27 13:48:23 +0200

    Changes

--- bin/collectstatic.sh
+++ bin/collectstatic.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-source /etc/bash_completion.d/virtualenvwrapper
 workon pmis
-
 cd /home/pmis/deploy
-
 # XXX: Test settings are the only ones I can get to work
 /home/pmis/.virtualenvs/pmis/bin/python manage.py collectstatic --settings=project.settings.test --noinput --clear
