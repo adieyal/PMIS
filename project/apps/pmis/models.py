517f9609b6e60e6c6d7ff680b90234150af70c60
Konstantin <moiseenko@steelkiwi.com>
2013-05-14 08:45:56 +0300

    Rename class Story to Versioned

--- project/apps/pmis/models.py
+++ project/apps/pmis/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from revisions.models import VersionedModel
 
 
-class Story(VersionedModel):
+class Versioned(VersionedModel):
     update_date = models.DateTimeField(default=datetime.datetime.now())
     update_comment = models.TextField()
     update_user = models.ForeignKey(User)
@@ -44,7 +44,7 @@ class Programme(models.Model):
         return self.name
 
 
-class Project(Story):
+class Project(Versioned):
     name = models.CharField(max_length=255)
     description = models.TextField()
     programme = models.ForeignKey(Programme, related_name='projects', null=True)
@@ -65,7 +65,7 @@ class ProjectPeople(models.Model):
     people_type = models.ManyToManyField(PeopleType, related_name='project_peoples')
 
 
-class ProjectFinancial(Story):
+class ProjectFinancial(Versioned):
     total_anticipated_cost = models.DecimalField(max_digits=20, decimal_places=2)
     project_planning_budget = models.DecimalField(max_digits=20, decimal_places=2)
     project = models.OneToOneField(Project)
@@ -74,7 +74,7 @@ class ProjectFinancial(Story):
         return u'Financial for %s' % self.project.name
 
 
-class Budget(Story):
+class Budget(Versioned):
     year = models.DateField()
     allocated_budget = models.DecimalField(max_digits=20, decimal_places=2)
     project_financial = models.ForeignKey(ProjectFinancial, related_name='budgets')
@@ -89,7 +89,7 @@ class ScopeCode(models.Model):
     code = models.CharField(max_length=255, unique=True, primary_key=True)
 
 
-class ScopeOfWork(Story):
+class ScopeOfWork(Versioned):
     quantity = models.PositiveIntegerField()
     scope_code = models.ForeignKey(ScopeCode, related_name='scope_of_works')
     project = models.ForeignKey(Project, related_name='scope_of_works')
@@ -98,7 +98,7 @@ class ScopeOfWork(Story):
         return u'Scope of work for %s' % self.project.name
 
 
-class Planning(Story):
+class Planning(Versioned):
     month = models.CharField(max_length=255)
     planned_expanses = models.PositiveIntegerField()
     planned_progress = models.PositiveIntegerField()
@@ -121,7 +121,7 @@ class Milestone(models.Model):
     project = models.ForeignKey(Project, related_name='milestones')
 
 
-class ProjectMilestone(Story):
+class ProjectMilestone(Versioned):
     completion_date = models.DateTimeField(default=datetime.datetime.now())
     milestone = models.ForeignKey(Milestone, related_name='project_milestone')
 
@@ -130,7 +130,7 @@ class CommentType(models.Model):
     name = models.CharField(max_length=255)
 
 
-class MonthlySubmission(Story):
+class MonthlySubmission(Versioned):
     month = models.DateField()
     current_milestone = models.ForeignKey(Milestone, related_name='monthly_submissions')
     actual_expenditure = models.PositiveIntegerField()
