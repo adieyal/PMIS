faa44231aecc8fa0ba4f1b9a0e2e16e266c1d221
Konstantin <moiseenko@steelkiwi.com>
2013-05-16 11:07:36 +0300

    Added a function to check if a user is defined groups for access to viewing the project.

--- project/apps/projects/views.py
+++ project/apps/projects/views.py
@@ -1,15 +1,14 @@
+from braces.views import LoginRequiredMixin
 from django.views.generic import DetailView
-from project.apps.projects.decorators import group_required
-from project.apps.projects.mixins import GroupRequiredMixin
 from project.apps.projects.models import Project
+from project.apps.projects.utils import group_required
 
 
-class ProjectView(DetailView):
+class ProjectView(LoginRequiredMixin, DetailView):
     model = Project
 
     def get_context_data(self, **kwargs):
-        print self.object.programme.client.name
-        group_required(self.object.programme.client.name)
+        group_required(self.request, self.object.programme.client.name, self.object.municipality.all()[0].district.name)
         return super(ProjectView, self).get_context_data(**kwargs)
 
 
