2258e21343ac8911823ec1ebef4f1faee6ed1bcd
Adi Eyal <adi@burgercom.co.za>
2013-06-09 21:17:06 +0200

    Added indentation for readability

--- front-end/app/js/app.js
+++ front-end/app/js/app.js
@@ -2,15 +2,16 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers'])
-.value('HOST','http://127.0.0.1:8000')
-.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.when('/api/projects/', {templateUrl: '/partials/projects.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/login/', {templateUrl: '/partials/login.html', controller: 'MyCtrl2'});
-    $routeProvider.when('/', {templateUrl: '/partials/home.html', controller: 'MyCtrl3'});
-    $routeProvider.when('/about/', {templateUrl: '/partials/about.html', controller: 'MyCtrl4'});
-    $routeProvider.when('/contact/', {templateUrl: '/partials/contact.html', controller: 'MyCtrl5'});
-    $routeProvider.when('/create_project/', {templateUrl: '/partials/form_create_project.html', controller: 'AppCtrl'});
-    $routeProvider.otherwise();
-    $locationProvider.html5Mode(true);
-}]);
+angular
+    .module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers'])
+    .value('HOST','http://127.0.0.1:8000')
+    .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+        $routeProvider.when('/api/projects/', {templateUrl: '/partials/projects.html', controller: 'MyCtrl1'});
+        $routeProvider.when('/login/', {templateUrl: '/partials/login.html', controller: 'MyCtrl2'});
+        $routeProvider.when('/', {templateUrl: '/partials/home.html', controller: 'MyCtrl3'});
+        $routeProvider.when('/about/', {templateUrl: '/partials/about.html', controller: 'MyCtrl4'});
+        $routeProvider.when('/contact/', {templateUrl: '/partials/contact.html', controller: 'MyCtrl5'});
+        $routeProvider.when('/create_project/', {templateUrl: '/partials/form_create_project.html', controller: 'AppCtrl'});
+        $routeProvider.otherwise();
+        $locationProvider.html5Mode(true);
+    }]);
