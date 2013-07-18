86d3eb03f6745719174e3c841e7300bc1d2c2957
Adi Eyal <adi@burgercom.co.za>
2013-07-18 09:54:16 +0200

    Can now process the entire json file

--- project/apps/projects/management/commands/load_projects.py
+++ project/apps/projects/management/commands/load_projects.py
@@ -1,4 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
+from dateutil import parser as dateparser
+from datetime import datetime
 from django.db import transaction
 import json
 from project.apps.projects import models
@@ -36,9 +38,117 @@ district_map = {
     "gert" : "Gert Sibande",
 }
 
+start_milestone = models.Milestone.objects.get(name="Project Identification")
+practical_completion_milestone = models.Milestone.objects.get(name="Practical Completion")
+final_completion_milestone = models.Milestone.objects.get(name="Final Completion")
+final_accounts_milestone = models.Milestone.objects.get(name="Final Accounts")
+
+contractor_role = models.Role.objects.get(name="Contractor")
+consultant_role = models.Role.objects.get(name="Consultant")
+
 class Command(BaseCommand):
     args = 'project json file'
     help = 'Loads projects from a json file'
+
+    def resolve_programme(self, programme):
+        programme =  programme_map[programme]
+        programme = models.Programme.objects.get(name=programme)
+        return programme
+
+    def resolve_municipality(self, district):
+        district = district_map[district.lower()]
+        return models.Municipality.objects.get(
+            name="Unknown",
+            district__name=district
+        )
+
+    def create_project(self, name, programme, municipality):
+        project, _ = models.Project.objects.get_or_create(
+            name=name, programme=programme,
+            municipality=municipality
+        )
+
+        return project
+
+    def create_project_financial(self, project, total_anticipated_cost):
+        financial, _ = models.ProjectFinancial.objects.get_or_create(
+            project=project
+        )
+
+        financial.total_anticipated_cost = total_anticipated_cost
+        financial.save()
+        return financial
+
+    def create_budget(self, project, year, total_budget, planning_budget):
+        budget, _ = models.Budget.objects.get_or_create(
+            year=year,
+            project=project
+        )
+        budget.allocated_budget = total_budget
+        budget.allocated_planning_budget = planning_budget
+        budget.save()
+
+        return budget
+
+    
+    def create_milestone(self, project, milestone, date):
+        milestone, _ = models.ProjectMilestone.objects.get_or_create(
+            project=project,
+            milestone=milestone
+        )
+        milestone.completion_date = date
+        milestone.save()
+        return milestone
+
+    def create_start_date(self, project, start_date):
+        return self.create_milestone(project, start_milestone, start_date)
+
+    def create_completion_dates(self, project, completion_dates):
+        self.create_milestone(project, practical_completion_milestone, completion_dates[0])
+        self.create_milestone(project, final_completion_milestone, completion_dates[1])
+        self.create_milestone(project, final_accounts_milestone, completion_dates[2])
+
+    def create_planning(self, project, year, month, planned_expenses, planned_progress):
+            try:
+                planning = models.Planning.objects.get(
+                    project=project, month=month, year=year
+                )
+            except models.Planning.DoesNotExist:
+                planning = models.Planning(project=project, month=month, year=year)
+
+            planning.planned_expenses = planned_expenses
+            planning.planned_progress = planned_progress
+            planning.save()
+            return planning
+
+    def create_monthly_submission(self, project, year, month, actual_expenses, actual_progress, comment, comment_type, remedial_action):
+            try:
+                submission = models.MonthlySubmission.objects.get(
+                    project=project, month=month, year=year
+                )
+            except models.MonthlySubmission.DoesNotExist:
+                submission = models.MonthlySubmission(project=project, month=month, year=year)
+
+            submission.actual_expenditure=actual_expenses
+            submission.actual_progress=actual_progress
+            submission.comment = comment
+            submission.remedial_action = remedial_action
+            submission.save()
+            return submission
+
+    def set_role(self, project, role, entity_name):
+        entity, _ = models.Entity.objects.get_or_create(name=entity_name)
+            
+        projectrole, _ = models.ProjectRole.objects.get_or_create(
+            project=project, role=role, entity=entity
+        )
+        return projectrole
+
+    def set_consultant(self, project, entity_name):
+        return self.set_role(project, consultant_role, entity_name)
+
+    def set_contractor(self, project, entity_name):
+        return self.set_role(project, contractor_role, entity_name)
 
     def handle(self, *args, **options):
         # Ensure that unknown municipalities are created
@@ -49,20 +159,37 @@ class Command(BaseCommand):
         data = json.load(open(args[0]))
         with transaction.commit_on_success():
             for datum in data:
-                programme = programme_map[datum["programme"]]
-                programme = models.Programme.objects.get(name=programme)
-                try:
-                    district = district_map[datum["district"].lower()]
-                except KeyError:
-                    # If there is an invalid district
-                    #import traceback
-                    #traceback.print_exc() 
-                    continue
-                municipality = models.Municipality.objects.get(
-                    name="Unknown",
-                    district__name=district
-                )
-                project = models.Project.objects.get_or_create(
-                    name=datum["project"], programme=programme,
-                    municipality=municipality
-                )
+                year = int(datum["year"])
+                programme = self.resolve_programme(datum["programme"])
+                municipality = self.resolve_municipality(datum["district"])
+
+                project = self.create_project(datum["project"], programme, municipality)
+
+                self.create_project_financial(project, float(datum["total_anticipated_cost"]))
+                self.create_budget(project, year, float(datum["allocated_budget"]), 0)
+                self.create_start_date(project, dateparser.parse(datum["start_date"]))
+                self.create_completion_dates(project, map(dateparser.parse, datum["revised_completion_dates"]))
+
+                months = zip([year - 1] * 9 + [year] * 3, models.financial_year)
+                for progress in datum["progress"]:
+                    self.create_planning(
+                        project, progress["year"], progress["month"],
+                        progress["planned_expenditure"], progress["planned_progress"]
+                    )
+                    if progress["year"] == datum["year"] and progress["month"] == datum["month"]:
+                        comment = datum["comment"]
+                        mitigation = datum["mitigation"]
+                    else:
+                        comment = mitigation = ""
+                    comment_type = None
+
+                    self.create_monthly_submission(
+                        project, progress["year"], progress["month"],
+                        progress["actual_expenditure"], progress["actual_progress"],
+                        comment, comment_type, mitigation
+                    )
+
+                self.set_contractor(project, datum["contractor"])
+                self.set_consultant(project, datum["consultant"])
+
+
