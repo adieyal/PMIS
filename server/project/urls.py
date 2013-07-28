f29dcfe408208326b62a337eca6f0ba53f82aba7
Adi Eyal <adi@burgercom.co.za>
2013-07-28 22:56:14 +0200

    Added district report assets

--- server/project/urls.py
+++ server/project/urls.py
@@ -18,12 +18,15 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^projects/', include('project.apps.projects.urls', namespace='projects')),
-    url(r'^api-token-auth/', 'rest_framework.authtoken.views.obtain_auth_token')
+    url(r'^api-token-auth/', 'rest_framework.authtoken.views.obtain_auth_token'),
+
+    url(r'^reports/district/dashboard/$', TemplateView.as_view(template_name='reports/district/index.html'), name='district_dashboard'),
+    url(r'^reports/district/progress/$', TemplateView.as_view(template_name='reports/district/progress.html'), name='district_progress'),
+    url(r'^reports/district/perform/$', TemplateView.as_view(template_name='reports/district/perform.html'), name='district_perform'),
 
     # url(r'^', include(router.urls)),
 )
