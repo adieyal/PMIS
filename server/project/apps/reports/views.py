2c9014bfb3cf1f7ef7211c468862ac4367904444
Adi Eyal <adi@burgercom.co.za>
2013-08-02 09:09:29 +0200

    moved old reports to reports2, new reports to reports

--- server/project/apps/reports/views.py
+++ server/project/apps/reports/views.py
@@ -6,10 +6,10 @@ def district_dashboard(request):
     # get json from api app
     # add json to response context
     # add variables into html direct
-    return TemplateResponse(request, 'reports2/district/index.html', district_report_json(2, 2013, 6))
+    return TemplateResponse(request, 'reports/district/index.html', district_report_json(2, 2013, 6))
 
 def district_progress(request):
-    return TemplateResponse(request, 'reports2/district/progress.html', {})
+    return TemplateResponse(request, 'reports/district/progress.html', {})
 
 def district_perform(request):
-    return TemplateResponse(request, 'reports2/district/perform.html', {})
+    return TemplateResponse(request, 'reports/district/perform.html', {})
