e59ca43535f229b109b1df9703856adac54b44a6
Adi Eyal <adi@burgercom.co.za>
2013-08-06 16:30:13 +0200

    Added test to reports app

--- server/project/apps/reports/views.py
+++ server/project/apps/reports/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from datetime import datetime
 from decimal import Decimal
 from collections import OrderedDict
 import json
@@ -7,11 +8,14 @@ from project.apps.api.reports.district_report import district_report_json
 from project.apps.projects import models
 
 def district_dashboard(request, district_id, year, month):
-    return TemplateResponse(request, 'reports/district/index.html', district_report_json(district_id, year, month))
+    date = datetime(int(year), int(month), 1)
+    return TemplateResponse(request, 'reports/district/index.html', district_report_json(district_id, date))
 
 def district_progress(request, district_id, year, month):
-    return TemplateResponse(request, 'reports/district/progress.html', district_report_json(district_id, year, month))
+    date = datetime(int(year), int(month), 1)
+    return TemplateResponse(request, 'reports/district/progress.html', district_report_json(district_id, date))
 
 def district_perform(request, district_id, year, month):
-    return TemplateResponse(request, 'reports/district/perform.html', {})
+    date = datetime(int(year), int(month), 1)
+    return TemplateResponse(request, 'reports/district/perform.html', district_report_json(district_id, date))
 
