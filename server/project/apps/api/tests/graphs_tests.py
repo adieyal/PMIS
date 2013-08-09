0b88eed103767f65b667177036de73bd081e8c65
Adi Eyal <adi@burgercom.co.za>
2013-08-09 15:27:15 +0200

    Finally managed to get the slider code test to pass

--- server/project/apps/api/tests/graphs_tests.py
+++ server/project/apps/api/tests/graphs_tests.py
@@ -14,12 +14,13 @@ class GraphJson(TestCase):
         self.year, self.month = 2013, 6
         self.url = reverse("api:reports:district_graphs", args=(self.district.id, self.year, self.month))
         self.date = datetime(self.year, self.month, 1)
+        self.budget = 100.
 
         for i in range(5):
             project = factories.ProjectFactory(municipality=self.municipality)
             submission = factories.MonthlySubmissionFactory(project=project, actual_progress=i*10, actual_expenditure=i*10, date=self.date)
             planning = factories.PlanningFactory(project=project, planned_progress=i*20, planned_expenses=i*20, date=self.date)
-            factories.ProjectFinancialFactory(project=project, total_anticipated_cost=10)
+            factories.ProjectFinancialFactory(project=project, total_anticipated_cost=self.budget)
 
         self.response = client.get(self.url)
         self.js = json.loads(self.response.content)
@@ -41,23 +42,22 @@ class GraphJson(TestCase):
     #        self.assertIn(pie, self.js)
         
 
-    # TODO re-enable
     def test_client_sliders(self):
-
+        budget = self.budget
+        min_value = 0.1 # The default min value in the slider code - if this test failed - check that this value is correct
         for i in range(0, 5):
             slider = "client_slider%d" % (i + 1)
             self.assertIn(slider, self.js)
-            if i == 0:
-                # Minimum value for the position is 0.1 regardless of the actual value
-                self.assertEqual(self.js[slider][0]["position"], 0.1)
-                self.assertEqual(self.js[slider][1]["position"], 0.1)
-    #        else:
-    #            # Values should swap around because the position of the sliders
-    #            # swaps if val1 > val 2
-    #            import pdb; pdb.set_trace()
-    #            self.assertEqual(self.js[slider][0]["position"], i / 10.)
-    #            self.assertEqual(self.js[slider][1]["position"], (i * 2) / 10.)
+            sliderjs = self.js[slider]
+            val = i * 10
+            planned = (val * 2) / self.budget if val > 0 else min_value
+            actual = val / self.budget if val > 10 else min_value
+
+            # planned and actual swap around because the markers swap around if val1 > val2
+            self.assertEqual(sliderjs[0]["position"], actual)
+            self.assertEqual(sliderjs[1]["position"], planned)
     # TODO add tests to test budget calculations for slider and also when budget is 0
+    # TODO add tests to test budget calculations for values greater than 1
 
 class TestGraphHelpers(TestCase):
     def test_dashboard_gauge(self):
@@ -70,18 +70,18 @@ class TestGraphHelpers(TestCase):
         self.assertEqual(gauge3[1]["text"], "def")
 
     def test_dashboard_slider(self):
-        slider1 = graphhelpers.dashboard_slider(0, 0, "DoE")
+        slider1 = graphhelpers.dashboard_slider(0, 0, "DoE", min_val=0.1, max_val=0.9)
         self.assertEqual(slider1[0]["position"], 0.1)
         self.assertEqual(slider1[1]["position"], 0.1)
 
-        slider2 = graphhelpers.dashboard_slider(1, 1, "DoE")
-        self.assertEqual(slider2[0]["position"], 0.8)
-        self.assertEqual(slider2[1]["position"], 0.8)
+        slider2 = graphhelpers.dashboard_slider(1, 1, "DoE", min_val=0.1, max_val=0.9)
+        self.assertEqual(slider2[0]["position"], 0.9)
+        self.assertEqual(slider2[1]["position"], 0.9)
 
-        slider3 = graphhelpers.dashboard_slider(0.5, 0.7, "DoE")
+        slider3 = graphhelpers.dashboard_slider(0.5, 0.7, "DoE", min_val=0.1, max_val=0.9)
         self.assertEqual(slider3[0]["marker-text"], "Planned")
         self.assertEqual(slider3[1]["marker-text"], "Actual")
-        slider3 = graphhelpers.dashboard_slider(0.5, 0.7, "DoE", text1="aaa", text2="bbb")
+        slider3 = graphhelpers.dashboard_slider(0.5, 0.7, "DoE", text1="aaa", text2="bbb", min_val=0.1, max_val=0.9)
         self.assertEqual(slider3[0]["marker-text"], "aaa")
         self.assertEqual(slider3[1]["marker-text"], "bbb")
 
@@ -89,15 +89,15 @@ class TestGraphHelpers(TestCase):
         self.assertEqual(slider4[0]["bar-color"], graphhelpers.clientcolors["DoE"])
         self.assertEqual(slider4[1]["bar-color"], graphhelpers.clientcolors["DoE"])
 
-        slider5 = graphhelpers.dashboard_slider(0.7, 0.5, "DoE", threshold=0.1)
+        slider5 = graphhelpers.dashboard_slider(0.7, 0.5, "DoE", threshold=0.1, min_val=0.1, max_val=0.9)
         self.assertEqual(slider5[1]["bar-color"], graphhelpers.red)
         self.assertEqual(slider5[0]["bar-color"], graphhelpers.clientcolors["DoE"])
 
-        slider6 = graphhelpers.dashboard_slider(0.5, 0.7, "DoE", threshold=0.1)
+        slider6 = graphhelpers.dashboard_slider(0.5, 0.7, "DoE", threshold=0.1, min_val=0.1, max_val=0.9)
         self.assertEqual(slider6[0]["bar-color"], graphhelpers.clientcolors["DoE"])
         self.assertEqual(slider6[1]["bar-color"], graphhelpers.red)
 
         
-        slider7 = graphhelpers.dashboard_slider(0.5, 0.55, "DoE", threshold=0.1)
+        slider7 = graphhelpers.dashboard_slider(0.5, 0.55, "DoE", threshold=0.1, min_val=0.1, max_val=0.9)
         self.assertEqual(slider7[0]["marker-text"], "")
 
