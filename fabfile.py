c0c1a1d3313676db819bb8faa81dd701e245d079
Adi Eyal <adi@burgercom.co.za>
2013-08-11 21:34:26 +0200

    Added create_database target in fabfile

--- fabfile.py
+++ fabfile.py
@@ -5,7 +5,9 @@ import urllib2
 
 api.env.hosts = ["adi@pmis.burgercom.co.za:2227"]
 trigger_url = "http://hudson.burgercom.co.za/job/PMIS/build"
-code_dir = "/var/www/PMIS/"
+code_dir = "/var/www/PMIS/server"
+env_dir = "/home/adi/.virtualenvs/pmis"
+python = "%s/bin/python" % env_dir
 
 def host_type():
     api.run('uname -s')
@@ -16,24 +18,27 @@ def commit():
     if result.failed and not confirm("Error with commit. Continue anyway?"):
         abort("Aborting at user request.")
 
-def create_database():
-    with api.cd(code_dir):
-        api.run("rm -f project/default.db")
-        api.run("python manage.py syncdb --noinput")
-        api.run("python manage.py migrate")
-        api.run("python manage.py loaddata production")
-        api.run("python manage.py createsuperuser")
-
 def test():
     api.local("server/manage.py test api projects")
 
 def push():
     api.local("git push")
 
-def prepare_deploy():
-    test()
-    commit()
-    push()
+
+def migrate():
+    with api.cd(code_dir):
+        api.run("%s manage.py migrate" % python)
+
+def create_database():
+    with api.cd(code_dir):
+        api.run("rm -f project/default.db")
+        api.run("%s manage.py syncdb --noinput" % python)
+        migrate()
+        api.run("%s manage.py loaddata production" % python)
+        api.run("%s manage.py createsuperuser" % python)
+        api.run("%s scripts/process_idp.py ../data/June.xls 2013 6 > /tmp/projects.json" % python)
+        api.run("%s manage.py load_projects /tmp/projects.json --traceback" % python)
+
 
 def deploy():
     """
