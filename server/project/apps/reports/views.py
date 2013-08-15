2df7fca8747b24e2aa358de6bb76efd4248382dd
Adi Eyal <adi@burgercom.co.za>
2013-08-15 13:56:20 +0200

    Fixed most of the issues with the index page
    Added different images for the maps in the logo
    Added arial narrow font
    Corrected the sliders at the top
    Fixed the date to use better month representations

--- server/project/apps/reports/views.py
+++ server/project/apps/reports/views.py
@@ -13,17 +13,17 @@ def test(request):
     district_report_json(2, date)
     return TemplateResponse(request, 'test.html', {})
 
-@cache_page(60 * 10)
+#@cache_page(60 * 10)
 def district_dashboard(request, district_id, year, month):
     date = datetime(int(year), int(month), 1)
     return TemplateResponse(request, 'reports/district/index.html', district_report_json(district_id, date))
 
-@cache_page(60 * 10)
+#@cache_page(60 * 10)
 def district_progress(request, district_id, year, month):
     date = datetime(int(year), int(month), 1)
     return TemplateResponse(request, 'reports/district/progress.html', district_report_json(district_id, date))
 
-@cache_page(60 * 10)
+#@cache_page(60 * 10)
 def district_perform(request, district_id, year, month):
     date = datetime(int(year), int(month), 1)
     return TemplateResponse(request, 'reports/district/perform.html', district_report_json(district_id, date))
