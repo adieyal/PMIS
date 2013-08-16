dfe8543a1de9f52d592653432e693d5506546e33
Adi Eyal <adi@burgercom.co.za>
2013-08-16 14:01:33 +0200

    Fixed cache for reports

--- server/project/apps/reports/views.py
+++ server/project/apps/reports/views.py
@@ -7,23 +7,24 @@ from django.template.response import TemplateResponse
 from project.apps.api.reports.district_report import district_report_json
 from project.apps.projects import models
 from django.views.decorators.cache import cache_page
+from django.conf import settings
 
 def test(request):
     date = datetime(2013, 6, 1)
     district_report_json(2, date)
     return TemplateResponse(request, 'test.html', {})
 
-#@cache_page(60 * 10)
+@cache_page(settings.API_CACHE)
 def district_dashboard(request, district_id, year, month):
     date = datetime(int(year), int(month), 1)
     return TemplateResponse(request, 'reports/district/index.html', district_report_json(district_id, date))
 
-#@cache_page(60 * 10)
+@cache_page(settings.API_CACHE)
 def district_progress(request, district_id, year, month):
     date = datetime(int(year), int(month), 1)
     return TemplateResponse(request, 'reports/district/progress.html', district_report_json(district_id, date))
 
-#@cache_page(60 * 10)
+@cache_page(settings.API_CACHE)
 def district_perform(request, district_id, year, month):
     date = datetime(int(year), int(month), 1)
     return TemplateResponse(request, 'reports/district/perform.html', district_report_json(district_id, date))
