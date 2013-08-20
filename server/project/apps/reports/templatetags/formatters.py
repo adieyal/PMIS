144a6ccf4748c51f4e04e1e36f2578e7ff7f5845
Adi Eyal <adi@burgercom.co.za>
2013-08-20 15:09:20 +0200

    Added format number template tag

--- server/project/apps/reports/templatetags/formatters.py
+++ server/project/apps/reports/templatetags/formatters.py
@@ -5,14 +5,22 @@ register = template.Library()
 
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
+# TODO - need to write a test for this
+def _format_number(value):
+    try:
+        if value == int(value):
+            return locale.format("%d", value, grouping=True)
+        else:
+            return locale.format("%.2f", value, grouping=True)
+    except ValueError:
+        return "-"
+
 def _format_currency(value):
     try:
         if int(value) == 0:
             return "R0.00"
-        elif value == int(value):
-            return "R" + locale.format("%d", value, grouping=True)
         else:
-            return "R" + locale.format("%.2f", value, grouping=True)
+            return "R" + _format_number(value)
     except ValueError:
         return "-"
 
@@ -25,11 +33,14 @@ def _format_percentage(value):
     except ValueError:
         return "-"
 
+@register.filter(name="format_number")
+def format_number(value):
+    return _format_number(value)
+
+@register.filter(name="format_currency")
 def format_currency(value):
     return _format_currency(value)
 
+@register.filter(name="format_percentage")
 def format_percentage(value):
     return _format_percentage(value)
-
-register.filter('format_currency', format_currency)
-register.filter('format_percentage', format_percentage)
