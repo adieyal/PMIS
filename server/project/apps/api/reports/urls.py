b483a1a595e76a700a8ad673f9124e4c58b0fe16
Etienne Kruger <etienne@acscomputers.co.za>
2013-10-16 00:53:44 +0200

    Added data code for head office reports.

--- server/project/apps/api/reports/urls.py
+++ server/project/apps/api/reports/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('project.apps.api.reports',
     url(r'^district/(?P<district_id>\d+)/(?P<year>\d+)/(?P<month>\d+)/$', 'district_report.district_report', name='district'),
     url(r'^district/(?P<district_id>\d+)/dashboard/(?P<year>\d+)/(?P<month>\d+)/graphs/$', 'district_report.dashboard_graphs', name='district_graphs'),
     url(r'^headoffice/dashboard/(?P<year>\d+)/(?P<month>\d+)/$', 'headoffice_report.headoffice_dashboard', name='headoffice_dashboard'),
+    url(r'^headoffice/progress/(?P<year>\d+)/(?P<month>\d+)/$', 'headoffice_report.headoffice_progress', name='headoffice_progress'),
+    url(r'^headoffice/performance/(?P<year>\d+)/(?P<month>\d+)/$', 'headoffice_report.headoffice_performance', name='headoffice_performance'),
 )
 
