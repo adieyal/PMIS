ab347ed04a262bc15454e6e317c0ac0dbfa36e8f
Etienne Kruger <etienne@acscomputers.co.za>
2014-03-22 23:28:18 +0200

    Fixes #40. Added fabfile for deployment.

---
+++ fabfile.py
@@ -0,0 +1,18 @@
+from fabric.api import *
+from fabric.contrib import *
+
+base_dir = '/var/www/PMIS'
+django_dir = base_dir + '/server'
+venv = '/home/adi/.virtualenvs/pmis/bin/activate'
+
+env.use_ssh_config = True
+env.hosts = ['pmis']
+
+def deploy():
+    with cd(base_dir):
+        run('git pull')
+    with cd(django_dir):
+        with prefix('source ' + venv):
+            run('python manage.py collectstatic --noinput')
+    with cd(base_dir):
+        sudo('supervisorctl restart pmis')
