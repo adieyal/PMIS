2647fc1cd0b047682a7bdc3f91f3ab1b426f36f8
Etienne Kruger <el@loadavg.io>
2014-09-18 09:49:22 +0200

    Fixes #112. Make progect.actual and project.planning default to pre populated list without data if they are empty strings.

--- server/project/apps/entry/views.py
+++ server/project/apps/entry/views.py
@@ -97,9 +97,35 @@ def edit(request, project_id):
         year += m/12
         month = m%12 + 1
         if project.acutal == '':
-            project.actual = []
+            project.actual = [
+                { 'expenditure': None, 'progress': None, 'date': '%04d-04-01T00:00:00' % (year) },
+                { 'expenditure': None, 'progress': None, 'date': '%04d-05-01T00:00:00' % (year) },
+                { 'expenditure': None, 'progress': None, 'date': '%04d-06-01T00:00:00' % (year) },
+                { 'expenditure': None, 'progress': None, 'date': '%04d-07-01T00:00:00' % (year) },
+                { 'expenditure': None, 'progress': None, 'date': '%04d-08-01T00:00:00' % (year) },
+                { 'expenditure': None, 'progress': None, 'date': '%04d-09-01T00:00:00' % (year) },
+                { 'expenditure': None, 'progress': None, 'date': '%04d-10-01T00:00:00' % (year) },
+                { 'expenditure': None, 'progress': None, 'date': '%04d-11-01T00:00:00' % (year) },
+                { 'expenditure': None, 'progress': None, 'date': '%04d-12-01T00:00:00' % (year) },
+                { 'expenditure': None, 'progress': None, 'date': '%04d-01-01T00:00:00' % (year+1) },
+                { 'expenditure': None, 'progress': None, 'date': '%04d-02-01T00:00:00' % (year+1) },
+                { 'expenditure': None, 'progress': None, 'date': '%04d-03-01T00:00:00' % (year+1) },
+            ]
         if project.planning == '':
-            project.planning = []
+            project.planning = [
+                { 'expenditure': None, 'progress': None, 'date': '%04d-04-01T00:00:00' % (year) },
+                { 'expenditure': None, 'progress': None, 'date': '%04d-05-01T00:00:00' % (year) },
+                { 'expenditure': None, 'progress': None, 'date': '%04d-06-01T00:00:00' % (year) },
+                { 'expenditure': None, 'progress': None, 'date': '%04d-07-01T00:00:00' % (year) },
+                { 'expenditure': None, 'progress': None, 'date': '%04d-08-01T00:00:00' % (year) },
+                { 'expenditure': None, 'progress': None, 'date': '%04d-09-01T00:00:00' % (year) },
+                { 'expenditure': None, 'progress': None, 'date': '%04d-10-01T00:00:00' % (year) },
+                { 'expenditure': None, 'progress': None, 'date': '%04d-11-01T00:00:00' % (year) },
+                { 'expenditure': None, 'progress': None, 'date': '%04d-12-01T00:00:00' % (year) },
+                { 'expenditure': None, 'progress': None, 'date': '%04d-01-01T00:00:00' % (year+1) },
+                { 'expenditure': None, 'progress': None, 'date': '%04d-02-01T00:00:00' % (year+1) },
+                { 'expenditure': None, 'progress': None, 'date': '%04d-03-01T00:00:00' % (year+1) },
+            ]
         _find_or_add_month(project.actual, year, month)
         _find_or_add_month(project.planning, year, month)
     
