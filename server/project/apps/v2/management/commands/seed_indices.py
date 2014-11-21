87131fc201b8085a55bbed6d105b4723ad4b5e75
datashaman <marlinf@datashaman.com>
2014-11-21 14:21:48 +0200

    Autocomplete mostly working

---
+++ server/project/apps/v2/management/commands/seed_indices.py
@@ -0,0 +1,67 @@
+import json as json
+from django.utils.text import slugify
+from django.core.management.base import BaseCommand, CommandError
+from project.libs.database.database import Project
+from elasticsearch import Elasticsearch
+from project.apps.reports.views import generate_cluster_dashboard_v2
+
+def make_null(project, field):
+    if field in project and project[field] == '':
+        project[field] = None
+
+es = Elasticsearch()
+
+class Command(BaseCommand):
+    help = 'Seed Elasticsearch indices from Redis'
+
+    clusters = [
+        "education",
+        "health",
+        "social-development",
+        "culture-sports-science-and-recreation",
+        "community-safety-security-and-liaison",
+        "economic-development-environment-and-tourism",
+    ]
+
+    date_fields = [
+        'planning_completion',
+        'planned_completion',
+        'planning_start',
+        'planned_start',
+        'actual_start',
+        'actual_completion',
+        'implementation_handover',
+        'revised_completion',
+    ]
+
+    def handle(self, *args, **options):
+        for c in self.clusters:
+            cluster = generate_cluster_dashboard_v2('department-of-%s' % c)
+            
+            for programme in cluster['programmes']:
+                if programme['name']:
+                    body = {
+                        'title': programme['name'],
+
+                        'cluster': cluster['client'],
+                        'cluster_id': c
+                    }
+
+                    programme_id = '%s:%s' % (c, slugify(programme['name']))
+                    es.index(index='pmis', doc_type='programme', id=programme_id, body=body)
+
+            for project_id in Project.list():
+                if project_id:
+                    project = Project.get(project_id, True)
+                    body = {
+                        'title': project['description'],
+
+                        'cluster': cluster['client'],
+                        'cluster_id': c,
+                        'manager': project.get('manager'),
+                        'municipality': project.get('municipality'),
+                        'comments': project.get('comments'),
+                        'programme': project.get('programme'),
+                        'programme_id': slugify(project.get('programme', u'')),
+                    }
+                    es.index(index='pmis', doc_type='project', id=project_id, body=body)
