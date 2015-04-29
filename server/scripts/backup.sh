22f6820efcb9c6a01dd62b7105346db83baefc5f
datashaman <marlinf@datashaman.com>
2015-04-29 01:03:20 -0400

    Add new version of MySQL-python (compilation problems on older version) and rewrite backup script

--- server/scripts/backup.sh
+++ server/scripts/backup.sh
@@ -1,4 +1,9 @@
-DBPATH=/var/www/PMIS/server/project/default.db
-BACKUPDIR=/var/backups/pmis
-FILENAME=default`date +"%Y%m%d"`.db
-cp $DBPATH $BACKUPDIR/$FILENAME
+DBPATH=/var/www/pmis/server/project/default.db
+BACKUPDIR=/var/backups
+DIR=`date +"%Y%m%d"`
+cd /tmp
+mkdir -p $DIR
+cp $DBPATH $DIR &&
+redis-dump > $DIR/redis-dump.json &&
+tar cvmpfz $BACKUPDIR/$DIR.tgz $DIR &&
+rm -r $DIR
